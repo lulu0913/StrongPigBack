1e403cad094d307be13717b930831b2db9970bb5
lulu0913 <1029390056@qq.com>
2019-12-29 17:24:42 +0800

    first commit

---
+++ src/main/java/com/javalsj/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.javalsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 项目启动类，启动程序的方法
+ *
+ */
+@SpringBootApplication
+public class BlogApplication 
+{
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
